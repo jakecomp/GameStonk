f3da1ffdd28d46891bfe570d76187c2eb87c4567
jakecomp <jakobvalen9@gmail.com>
2024-09-20 14:45:04 -0700

    Fetch live stock prices, setup secure rails credentials

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,16 @@
 class StocksController < ApplicationController
+  include StocksHelper
   def index
     @stocks = Stock.all
   end
 
   def show
     @stock = Stock.find(params[:id])
+    price = getStockPrice(@stock.shorthand)
+    @stock.price = price
+  end 
+
+  def new
+    @stock = Stock.new
   end
 end
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,10 @@
 module StocksHelper
+  def getStockPrice(symbol)
+     url = 'https://api.twelvedata.com/time_series?symbol='+symbol.to_s+"&interval=1min&apikey="+Rails.application.credentials.twelvedata.api_key
+     response = HTTParty.get(url)
+
+     if response["status"] == "ok"
+       return response["values"][0]["open"]
+     end
+  end
 end
